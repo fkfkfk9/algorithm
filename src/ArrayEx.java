5306a7f9908217032cb3c03cd45c39c0566bc528
fkfkfk9 <fkfkfk999@gmail.com>
2021-08-22 23:47:45 +0900

    Initial commit

---
+++ src/ArrayEx.java
@@ -0,0 +1,14 @@
+public class ArrayEx {
+    public static void main(String[] args) {
+        /* 넘어온 배열에 중복되는 숫자가 있다면 ture, 없다면 false */
+
+    }
+
+    private boolean arrayDuplicateCheckNum(Int[] numArr){
+        boolean result = false;
+
+
+
+        return result;
+    }
+}
