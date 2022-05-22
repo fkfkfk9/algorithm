63e1ae754878557b78b5c05770304560ca98c34b
fkfkfk9 <fkfkfk999@gmail.com>
2022-05-23 01:06:46 +0900

    220522 별그리기

---
+++ src/DrawSquareWithStar.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class DrawSquareWithStar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for (int i = 1; i <= a*b; i++) {
+            System.out.print("*");
+            if(i % a == 0){
+                System.out.print("\n");
+            }
+        }
+    }
+}
